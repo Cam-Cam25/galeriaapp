26d8accf49318d82818f462c6d464601ac9f63fd
camcamguaman25 <camcamguaman25@gmail.com>
2025-03-05 20:18:20 +0000

    hola2

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'myapp',
-  webDir: 'dist'
+  webDir: 'dist/myapp/browser',
+
 };
 
 export default config;
